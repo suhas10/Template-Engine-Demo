2c06b88e59dc9d24a982155d65f9464f9b6a24ad
suhas10 <suhas.pindiproli@johnlewis.co.uk>
2019-09-13 15:24:50 +0530

    Connecting View to Backend

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const expressHandleBars = require('express-handlebars')
 const app = express();
+var bodyParser = require('body-parser');
 const path = require('path');
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('handlebars', expressHandleBars());
+app.set('view engine', 'handlebars');
 
-app.get('/template', (req, res) => {
-    res.render('template')
+app.get('/', (req, res) => {
+    res.render('home')
 })
 
+app.post('/contact', (req, res) => {
+    let data = {
+        name: req.body.name,
+        age: req.body.age,
+        contact_no: req.body.contact_no,
+        address: req.body.address
+    }
+
+    console.log(data);
+})
 module.exports = app
